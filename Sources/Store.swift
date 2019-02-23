1f60c9b36807cffdc01c6119022d3506a030fc83
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-02-22 20:01:42 -0600

    Didn’t like that

--- Sources/Store.swift
+++ Sources/Store.swift
@@ -7,7 +7,7 @@ public protocol State {
 
 public protocol Action {
     associatedtype State: Prestige.State
-    func mutate(_ state: State, completion: @escaping (Result<State>) -> Void)
+    func mutate(_ state: State) -> Promise<State>
 }
 
 public final class Store<State: Prestige.State> {
@@ -18,15 +18,8 @@ public final class Store<State: Prestige.State> {
     }
     
     public func dispatch<Action: Prestige.Action>(_ action: Action) -> Promise<Void> where Action.State == State {
-        return Promise { seal in
-            action.mutate(self.state) { result in
-                switch result {
-                case .fulfilled:
-                    seal.fulfill(())
-                case let .rejected(error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return action.mutate(self.state)
+        .get { self.state = $0 }
+        .asVoid()
     }
 }
