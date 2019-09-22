6f8ba87fe5154a7e5054b1f0df77a859d9bab3e1
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-09-22 13:56:57 -0500

    commented out test, updated proj file

--- Tests/PrestigeTests/PrestigeTests.swift
+++ Tests/PrestigeTests/PrestigeTests.swift
@@ -6,7 +6,7 @@ final class PrestigeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Prestige().text, "Hello, World!")
+//        XCTAssertEqual(Prestige().text, "Hello, World!")
     }
 
     static var allTests = [
