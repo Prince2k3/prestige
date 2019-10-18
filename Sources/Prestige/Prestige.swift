d556cdf99166302d00a8990e410b2c7b86c333e1
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-10-18 17:42:01 -0500

    updates
    updates to the combine version of prestige,
    prestige now does commit for  non async mutation of state

--- Sources/Prestige/Prestige.swift
+++ Sources/Prestige/Prestige.swift
@@ -10,6 +10,11 @@ public protocol State {
     init()
 }
 
+public protocol Mutation {
+    associatedtype S: State
+    func mutate(_ state: S) -> S
+}
+
 #if canImport(PromiseKit)
 
 public protocol Action {
@@ -24,6 +29,10 @@ public final class Store<S: Prestige.State> {
         self.state = state
     }
     
+    public func commit<M: Mutation>(_ mutation: M) where M.S == S {
+        self.state = mutation.mutate(state)
+    }
+    
     public func dispatch<A: Prestige.Action>(_ action: A) -> Promise<Void> where A.S == S {
         return action.mutate(state)
         .get { self.state = $0 }
@@ -35,43 +44,41 @@ public final class Store<S: Prestige.State> {
 
 public protocol Action {
     associatedtype S: State
-    func mutate(_ state: S) -> AnyPublisher<S, Error>
+    func mutate(_ state: S) -> Future<S, Error>
 }
 
+@dynamicMemberLookup
 public final class Store<S: State>: ObservableObject {
     private var subject: CurrentValueSubject<S, Error>
-    private var cancellable: Cancellable?
-    
-    @Published public var state: S
-    
-    deinit {
-        cancellable?.cancel()
-        cancellable = nil
-    }
-    
+
     public init(state: S) {
-        self.state = state
         self.subject = CurrentValueSubject(state)
     }
-    
-    public func cancel() {
-        cancellable?.cancel()
-        cancellable = nil
-    }
 
-    public func dispatch<A: Prestige.Action>(_ action: A) where A.S == S {
-        cancellable = action.mutate(state)
+    public func dispatch<A: Action>(_ action: A) where A.S == S {
+        action.mutate(subject.value)
         .sink(receiveCompletion: { result in
             if case let .failure(error) = result {
                 self.subject.send(completion: .failure(error))
             }
         }, receiveValue: { s in
-            DispatchQueue.main.async {
-                self.state = s
-            }
-            
             self.subject.send(s)
         })
+    }
+    
+    public func commit<M: Mutation>(_ mutation: M) where M.S == S {
+        Just(mutation.mutate(subject.value))
+        .sink(receiveCompletion: { result in
+            if case let .failure(error) = result {
+                self.subject.send(completion: .failure(error))
+            }
+        }, receiveValue: { s in
+            self.subject.send(s)
+        })
+    }
+    
+    subscript<U>(dynamicMember keyPath: KeyPath<S, U>) -> U {
+        subject.value[keyPath: keyPath]
     }
 
     public func observe<T: Equatable>(_ keyPath: KeyPath<S, T>) -> AnyPublisher<T, Error> {
